8f242cc7a162b78eb4caa23c1aced1208820977f
stahlhammer88 <stahlhammer88@gmail.com>
2019-05-22 17:13:37 +0300

    cleaned up

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ import Tour from './components/Tour';
 import Shop from './components/shop/Shop';
 import Footer from './components/header-footer/Footer';
 import Header from './components/header-footer/Header';
-//import DropDown from './ui-dropdown/DropDown';
-import Upload from './upload/Upload';
 
 const Routes = () => (
     <BrowserRouter>
@@ -26,4 +24,4 @@ const Routes = () => (
     </BrowserRouter>
 )
 
-ReactDOM.render(<Upload/>, document.getElementById('root'));
+ReactDOM.render(<Routes/>, document.getElementById('root'));
